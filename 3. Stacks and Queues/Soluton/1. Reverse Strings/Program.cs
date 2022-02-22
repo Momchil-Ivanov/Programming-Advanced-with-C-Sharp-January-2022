9a9dd75ba730f55f3953c9fdf8e9194b48755868
Momchil Ivanov <88820552+Momchil-Ivanov@users.noreply.github.com>
2022-02-22 17:47:55 +0200

    Add files via upload

---
+++ 3. Stacks and Queues/Soluton/1. Reverse Strings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stacks_and_Queues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringToReverse = Console.ReadLine();
+
+            Stack<char> stack = new Stack<char>();
+
+            foreach (var symbol in stringToReverse)
+            {
+                stack.Push(symbol);
+            }
+
+            while (stack.Count > 0)
+            {
+                Console.Write(stack.Pop());
+            }
+        }
+    }
+}
---
+++ 3. Stacks and Queues/Soluton/2. Stack Sum/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2._Stack_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> stack = new Stack<int>(numbers);
+
+            string command = Console.ReadLine().ToLower();
+
+            while (command != "end")
+            {
+                var splitedCommand = command.Split();
+                if (splitedCommand[0] == "add")
+                {
+                    int first = int.Parse(splitedCommand[1]);   
+                    int second = int.Parse(splitedCommand[2]);
+
+                    stack.Push(first);
+                    stack.Push(second);
+                }
+                if (splitedCommand[0] == "remove")
+                {
+                    int count = int.Parse(splitedCommand[1]);
+                    if (count <= stack.Count)
+                    {
+                        for (int i = 0; i < count; i++)
+                        {
+                            stack.Pop();
+                        }
+                    }
+                }
+                command = Console.ReadLine().ToLower();
+            }
+
+            int sum = 0;
+            while (stack.Count>0)
+            {
+                sum += stack.Pop();
+            }
+
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
