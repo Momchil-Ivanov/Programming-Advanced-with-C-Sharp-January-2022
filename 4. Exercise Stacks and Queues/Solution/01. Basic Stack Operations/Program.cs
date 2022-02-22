ace928670be681601925b0f8310e3dc782b1e8c0
Momchil Ivanov <88820552+Momchil-Ivanov@users.noreply.github.com>
2022-02-22 17:55:03 +0200

    Add files via upload

---
+++ 4. Exercise Stacks and Queues/Solution/01. Basic Stack Operations/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise_Stacks_and_Queues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] command = Console.ReadLine().Split(' ');
+            int toPush = int.Parse(command[0]);
+            int toPop = int.Parse(command[1]);
+            int toLookFor = int.Parse(command[2]);
+
+            string[] numbers = Console.ReadLine().Split(' ');
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < toPush; i++)
+            {
+                int number = int.Parse(numbers[i]);
+                stack.Push(number);
+            }
+
+            for (int i = 0; i < toPop; i++)
+            {
+                stack.Pop();
+            }
+
+            if (stack.Contains(toLookFor))
+            {
+                Console.WriteLine($"true");
+            }
+            else if (!stack.Contains(toLookFor) && stack.Count > 0)
+            {
+                int[] arr = stack.ToArray();
+                int smallest = int.MaxValue;
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (smallest > arr[i])
+                    {
+                        smallest = arr[i];
+                    }
+                }
+                Console.WriteLine(smallest);
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+    }
+}
---
+++ 4. Exercise Stacks and Queues/Solution/02. Basic Queue Operations/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Basic_Queue_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>();
+            int[] commands = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int toAdd = commands[0];
+            int toRemove = commands[1];
+            int toLookFor = commands[2];
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            for (int i = 0; i < toAdd; i++)
+            {
+                queue.Enqueue(numbers[i]);
+            }
+            for (int i = 0; i < toRemove; i++)
+            {
+                queue.Dequeue();
+            }
+            if (queue.Contains(toLookFor))
+            {
+                Console.WriteLine($"true");
+            }
+            else if (!queue.Contains(toLookFor) && queue.Count > 0)
+            {
+                Console.WriteLine(queue.Min());
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+    }
+}
