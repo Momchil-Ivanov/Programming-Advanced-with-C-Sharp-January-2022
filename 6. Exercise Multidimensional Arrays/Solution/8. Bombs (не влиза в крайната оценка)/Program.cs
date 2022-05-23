5c076f222b7aa022664b1ebd2ecf6fd7e36d5a57
Momchil Ivanov <88820552+Momchil-Ivanov@users.noreply.github.com>
2022-05-23 13:24:27 +0300

    Add files via upload

---
+++ 6. Exercise Multidimensional Arrays/Solution/8. Bombs (не влиза в крайната оценка)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _8._Bombs__не_влиза_в_крайната_оценка_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
